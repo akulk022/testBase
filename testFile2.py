9300d8a7a70c977b2b144b98da0475607b4c88fb
Abhijit Kulkarni <akulk022@ucr.edu>
2025-04-10 21:46:59 -0700

    Sample Bug Inducing Commit

--- testFile2.py
+++ testFile2.py
@@ -1,9 +1,19 @@
+import os
 import json
 
-#This fixes the insecure deserialization vulnerability.
+# SAFE: Fix for insecure deserialization
 def load_user_data(data):
     return json.loads(data)
 
-#take user input
-user_input = input("Paste your serialized data (JSON): ")
-user_data = load_user_data(user_input)
+# VULNERABLE: Command injection via unsanitized input
+def run_command(user_input):
+    # This is unsafe: user can inject arbitrary shell commands
+    os.system("echo Hello && " + user_input)
+
+# USAGE
+if __name__ == "__main__":
+    user_data_input = input("Paste your serialized data (JSON): ")
+    user_data = load_user_data(user_data_input)
+    
+    command_input = input("Enter command to run: ")
+    run_command(command_input)
